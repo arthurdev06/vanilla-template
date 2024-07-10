86aa3bdb7dd42b401d65ba007fccaf285f19b000
Arthur Rocha <116519847+arthurdev06@users.noreply.github.com>
2024-07-10 11:07:26 -0300

    create plugin structure

--- lua/vanilla-template/init.lua
+++ lua/vanilla-template/init.lua
@@ -1,7 +1,48 @@
 local M = {}
 
+local function write_file(file_path, content)
+	local file = io.open(file_path, "w")
+	file:write(content)
+	file:close()
+end
+
+local function create_files()
+	local html_content = [[
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <script defer src="script.js"></script>
+  <title>Document</title>
+</head>
+<body>
+  
+  
+</body>
+</html>
+  ]]
+
+	local css_content = [[
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+  ]]
+
+	local js_content = [[
+  "use strict";
+  ]]
+
+	write_file("index.html", html_content)
+	write_file("styles.css", css_content)
+	write_file("script.js", js_content)
+end
+
 function M.setup()
-	print("test")
+	vim.api.nvim_create_user_command("vanilla-template", create_files, {})
 end
 
 return M
